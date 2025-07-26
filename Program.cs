4818eb0d0bd67ed87038e4b5d12b78adbc83d3cd
Mustafa Bahomaid <mustafabahomaid@gmail.com>
2025-07-26 14:30:13 +0300

    feat(calculator): build the calculator

--- Program.cs
+++ Program.cs
@@ -4,6 +4,55 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello mathematician, choose operation: ");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+
+            string? choice = Console.ReadLine();
+            if (choice is null)
+            {
+                Console.WriteLine("Invalid choice, exiting.");
+                return;
+            }
+            Console.WriteLine("Enter first number: ");
+            string? firstInput = Console.ReadLine();
+            Console.WriteLine("Enter second number: ");
+            string? secondInput = Console.ReadLine();
+
+            if (firstInput is null || secondInput is null)
+            {
+                Console.WriteLine("Invalid input, exiting.");
+                return;
+            }
+
+            int? firstNumber = int.Parse(firstInput);
+            int? secondNumber = int.Parse(secondInput);
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"Result: {firstNumber + secondNumber}");
+                    break;
+                case "2":
+                    Console.WriteLine($"Result: {firstNumber - secondNumber}");
+                    break;
+                case "3":
+                    Console.WriteLine($"Result: {firstNumber * secondNumber}");
+                    break;
+                case "4":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero, exiting.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Result: {firstNumber / secondNumber}");
+                    }
+                    break;
+
+            }
+
 
         }
     }
